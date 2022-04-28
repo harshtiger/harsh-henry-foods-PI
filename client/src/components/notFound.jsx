4ce748fe29d7421acda79765ab664c30badf9c9c
harshtiger <ttiger6666@gmail.com>
2022-04-28 01:16:49 -0300

    added a 404 page

---
+++ client/src/components/notFound.jsx
@@ -0,0 +1,25 @@
+
+import React from "react";
+//"../styles/NotFound.css";
+import { Link } from "react-router-dom";
+
+export default function LandingPage() {
+    return (
+      <div className="notFound">
+        <h1>Oh! seems we got lost</h1>
+  
+        <img src="https://i.ibb.co/VC2NbrX/404harsh.png" alt="404harsh" border="0" />
+        
+          <h2>Don't worry, click the button bellow and we'll be back home!</h2>
+         
+        
+          <div>
+          <Link to="./home" className="btnNF">
+            <button>Here!</button>
+          </Link>
+        </div>
+        
+      </div>
+    );
+  }
+  
