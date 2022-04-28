25b2ef16203449b93d75a1dbd1d8eb350af9fad7
harshtiger <ttiger6666@gmail.com>
2022-04-28 01:20:35 -0300

    added a 404 component and its css file

--- client/src/components/notFound.jsx
+++ client/src/components/notFound.jsx
@@ -2,7 +2,7 @@
 import React from "react";
 //"../styles/NotFound.css";
 import { Link } from "react-router-dom";
-
+//owo
 export default function LandingPage() {
     return (
       <div className="notFound">
