3a17155bfe16adb02692d4c982fa4baf073f239e
harshtiger <ttiger6666@gmail.com>
2022-05-07 10:49:24 -0300

    added a PUT route, back end is fully crud now

---
+++ api/src/controllers/updateRecipe.js
@@ -0,0 +1,27 @@
+const {Recipe}= require("../db")
+
+const putRecipe = async (req, res) => {
+    try {
+      const { id } = req.params;
+      let updatedRecipe = await Recipe.findOne({
+        where: {
+          id: id,
+        },
+      });
+      await updatedRecipe.update({
+        title: req.body.title,
+        summary: req.body.summary,
+        aggregateLikes: req.body.aggregateLikes,
+        healthScore: req.body.healthScore,
+        analyzedInstructions: req.body.analyzedInstructions,
+        image: req.body.image,
+        diets: req.body.diets,
+       
+        });
+      
+      res.send(updatedRecipe);
+    } catch (error) {
+      res.status(400).send(error);
+    }
+  };
+  module.exports = putRecipe
